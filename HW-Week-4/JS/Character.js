044cacaf18f4a598ec4006d33ea269ce133cd4ac
Rainer wood <112429766+RaineoftheWood@users.noreply.github.com>
2023-02-14 10:34:36 -0700

    aHHHHHH

---
+++ HW-Week-4/JS/Character.js
@@ -0,0 +1,22 @@
+class CharacterPump{
+        constructor(path, x, y)
+        {
+            this.path = path;
+            this.x = x;
+            this.y = y;
+        }
+    }
+    
+    getImage()
+    {
+        var myImage = loadImage(this.path);
+        return myImage;
+    }
+    getX()
+    {
+        return this.x;
+    }
+    getY()
+    {
+        return this.y;
+    }
