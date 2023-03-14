8a632130be6fb751f244b6d0629b8d74ce8ca902
Rainer wood <112429766+RaineoftheWood@users.noreply.github.com>
2023-03-13 21:27:02 -0600

    Update

--- HW-Week-8/JS/HW8-Sketch.js
+++ HW-Week-8/JS/HW8-Sketch.js
@@ -9,8 +9,8 @@ function setup() {
 	coin = new Sprite(400, 500, 30);
 	coin.color = 'yellow';
 	sprite1 = new Sprite(50, 50);
-    sprite1.addAni('idle','./IMAGES/idle/Idle (1).png', './IMAGES/idle/Idle (16).png');
-    sprite1.addAni('walk', './IMAGES/walk/Walk (1).png', './IMAGES/walk/Walk (20).png')
+    //sprite1.addAni('idle','./IMAGES/idle/Idle (1).png', './IMAGES/idle/Idle (16).png');
+    //sprite1.addAni('walk', './IMAGES/walk/Walk (1).png', './IMAGES/walk/Walk (20).png')
     sprite2 = new Sprite(500, 500);
     sprite3 = new Sprite( 800, 30, 60, 4);
     wall1 = new Sprite(990, 0, 10, 1500, 's');
@@ -28,7 +28,7 @@ function setup() {
 	gems.amount = 80;
     sprite1.overlaps(gems, collect)
     
-    if (kb.pressing('left')) {
+    /*if (kb.pressing('left')) {
 		sprite1.ani = 'walk';
 		sprite1.vel.x = -2;
 		sprite1.mirror.x = true;
@@ -40,7 +40,7 @@ function setup() {
 		sprite1.ani = 'idle';
 		sprite1.vel.x = 0;
 	}
-
+*/
 
 
 }
@@ -52,7 +52,7 @@ function collect(sprite1, gem) {
 function draw() {
 	clear();
     //animation(ghostAni,300, 300);
-	//sprite1.moveTowards(mouse);
+	sprite1.moveTowards(mouse);
 		
 	if (sprite1.overlaps(coin)) coin.remove();
     if (kb.pressing('space')) {
